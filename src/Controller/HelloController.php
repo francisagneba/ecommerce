e08e9adc98626df1af508efdc88615c1de83c3de
fagneba <francis.agneba@gmail.com>
2024-08-24 05:54:34 +0200

    Ajout de template grace à Bootswatch

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,32 +2,21 @@
 
 namespace App\Controller;
 
-use App\Taxes\Calculator;
-use App\Taxes\Detector;
-use Cocur\Slugify\Slugify;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
-class HelloController
+
+class HelloController extends AbstractController
 {
 
     /**
      * @Route("/hello/{prenom?Word}", name="hello")
      */
-    public function hello(string $prenom, Environment $twig): Response
+    public function hello(string $prenom)
     {
-        $html = $twig->render('hello.html.twig', [
+        return $this->render('hello.html.twig', [
             'prenom' => $prenom,
-            'ages' => [
-                12,
-                18,
-                29,
-                15
-            ]
 
         ]);
-        return new Response($html);
     }
 }
