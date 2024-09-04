55b237ab53e755a8e5b1e6b609c6e27beb02d031
fagneba <francis.agneba@gmail.com>
2024-09-04 23:57:58 +0200

    Mise en place de l'authentification

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,24 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Category;
+use Faker\Factory;
+use App\Entity\User;
 use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Faker\Factory;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    protected $slugger;
+    private $slugger;
+    private $passwordHasher;
 
-    public function __construct(SluggerInterface $slugger)
+    public function __construct(SluggerInterface $slugger, UserPasswordHasherInterface $passwordHasher)
     {
         $this->slugger = $slugger;
+        $this->passwordHasher = $passwordHasher;
     }
 
     public function load(ObjectManager $manager): void
@@ -24,9 +28,30 @@ class AppFixtures extends Fixture
         $faker->addProvider(new \Liior\Faker\Prices($faker));
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
 
+        $admin = new User();
+
+        $hash = $this->passwordHasher->hashPassword($admin, "password");
+
+        $admin->setEmail("admin@gmail.com")
+            ->setFullName("Admin")
+            ->setPassword($hash)
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+            $hash = $this->passwordHasher->hashPassword($user, "password");
+            $user->setEmail("user$u@gmail.com")
+                ->setFullName($faker->name())
+                ->setPassword($hash);
+
+            $manager->persist($user);
+        }
+
         for ($c = 0; $c < 6; $c++) {
             $category = new Category();
-            $categoryName = ucfirst(implode(' ', $faker->words(2))); // Génère un nom de categorie en combinant 3 mots
+            $categoryName = ucfirst(implode(' ', $faker->words(2)));
             $category->setName($categoryName)
                 ->setSlug(strtolower($this->slugger->slug($categoryName)));
 
@@ -34,7 +59,7 @@ class AppFixtures extends Fixture
 
             for ($p = 0; $p < mt_rand(15, 20); $p++) {
                 $product = new Product();
-                $productName = ucfirst(implode(' ', $faker->words(3))); // Génère un nom de produit en combinant 3 mots
+                $productName = ucfirst(implode(' ', $faker->words(3)));
                 $product->setName($productName)
                     ->setPrice($faker->price(4000, 20000))
                     ->setSlug($this->slugger->slug($productName))
